ad0d391ecad1afe05ab9971f5c85ecdd4069f683
Gerónimo <gerolucas@gmail.com>
2024-09-01 23:00:40 -0300

    1er Ejercicio ParteUno listo, 2do en camino.

---
+++ EntregableParteUno.js
@@ -0,0 +1,26 @@
+// Challenge `sayGoodbye`
+
+function sayGoodbye(nombre){
+  console.log ('Adiós, ' + nombre + '.Que tengas un buen día');
+}
+
+sayGoodbye("Andy")
+
+// Challenge `temperatureInC`
+
+function convertirACelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+ 
+convertirACelsius(32)
+
+// Ejemplo de uso
+const fahrenheit = 52; // Ejemplo de temperatura en Fahrenheit
+const celsius = convertirACelsius(fahrenheit) && 0;  
+console.log(`${fahrenheit} grados Fahrenheit son ${celsius.toFixed(0)} grados Celsius.`);
+
+
+
+function temperatureInC(grado, escala){
+  return (`${fahrenheit} grados Fahrenheit son ${celsius.toFixed(0)} grados Celsius.`)
+}
